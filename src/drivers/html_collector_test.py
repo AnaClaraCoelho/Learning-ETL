df5e61547953cb0c0c6838fb4e9e3b21553e3d66
ana.coelho <ana.coelho@buser.com.br>
2023-02-24 15:54:40 -0300

    feat: implementing html_collector with tests and http_requester_mock

---
+++ src/drivers/html_collector_test.py
@@ -0,0 +1,14 @@
+from .html_collector import HtmlCollector
+from .mocks.http_requester_mock import mock_request_from_pages
+
+def test_collect_essential_information():
+    http_request_response = mock_request_from_pages()
+
+    html_collector = HtmlCollector()
+    essential_information = html_collector.collect_essential_information(http_request_response["html"])
+
+    assert isinstance(essential_information, list)
+    assert isinstance(essential_information[0], dict)
+    assert 'name' in essential_information[0]
+    assert 'link' in essential_information[0]
+    
